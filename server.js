3984d0859f13f802a6838ab000cc85bd13787d72
Forger <info@forger.com.ar>
2026-05-29 23:59:38 -0300

    connect-tn: logo TiendaNube inline (SVG navy) en la pantalla de confirmación
    El favicon externo (tiendanube.com/favicon.ico) no cargaba → círculo vacío en
    el trío. Reemplazado por SVG inline #2C3357, sin dependencia externa.

--- server.js
+++ server.js
@@ -556,7 +556,7 @@ function confirmationPage({ payload, returnTo, nonce, siteUrl, alreadyConnected 
                 <span class="line"></span><span class="dot"></span><span class="dot d2"></span><span class="dot d3"></span>
             </div>
             <div class="side">
-                <div class="ring"><img src="https://www.tiendanube.com/favicon.ico" alt="TiendaNube" onerror="this.style.display='none'" /></div>
+                <div class="ring"><svg width="34" height="34" viewBox="0 0 16 16" fill="#2C3357" xmlns="http://www.w3.org/2000/svg" aria-label="TiendaNube"><path d="M10.25 2.24a5.79 5.79 0 0 0-4 1.63 4.48 4.48 0 1 0 0 8.26 5.76 5.76 0 1 0 4-9.89zm0 10.24A4.49 4.49 0 0 1 5.76 8H4.48a5.74 5.74 0 0 0 .89 3.07 3.29 3.29 0 0 1-.88.13 3.2 3.2 0 0 1 0-6.4A3.2 3.2 0 0 1 7.69 8H9a4.42 4.42 0 0 0-1.63-3.43 4.48 4.48 0 1 1 2.88 7.91z"/></svg></div>
                 <span class="label">Tu tienda<br>en TiendaNube</span>
             </div>
         </div>
